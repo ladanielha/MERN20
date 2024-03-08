cf2d5516ca25b4c2f3fb111b11f6bae7334df89b
ladanielha <laurentiusdaniel0@gmail.com>
2024-03-08 19:34:28 +0700

    day10

---
+++ Day10/Class/class/class.js
@@ -0,0 +1,20 @@
+class Car {
+    constructor(name){
+        this.name = name;
+    }
+
+    tampil(){
+        return this.name
+    }
+}
+
+class Sport extends Car {
+    tujuan() {
+        return `Mobil ${this.name} untuk pribadi`
+    }
+}
+
+const Ford = new Sport("Ford");
+document.querySelector("h1").innerText = Ford.tampil()
+console.log(Ford.tampil())
+console.log(Ford.tujuan())
